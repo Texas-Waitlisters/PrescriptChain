66c3ada12c6e9ece8c9a799de8484e6010157034
Akash Kwatra <akashkw@gmail.com>
2018-06-09 21:36:48 -0500

    refactor unit tests

--- factom.py
+++ factom.py
@@ -11,16 +11,23 @@ KEY = 'GUWiB4x3JwrOiaWyFMYLlRmfJr7YfFFGDMzwYG80sdtL4BDT'
 
 HEADER  = {'Content-Type': 'application/json', 'factom-provider-token': KEY }
 
-def _base64(str):
+# Return base 64 encoding of a string
+def _b64encode(str):
     return base64.b64encode(str.encode('utf-8')).decode('utf-8')
 
-def unix_timestamp():
+# Return base 64 decoding of a string
+def _b64decode(str):
+    return base64.b64decode(str.encode('utf-8')).decode('utf-8')
+
+# Return unix timestamp at current moment
+def _unix_timestamp():
     return "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
 
+# Create json payload for a block using external_ids and content
 def _json_block_payload(external_ids, content):
     payload = dict()
-    payload["external_ids"] = [_base64(_id) for _id in external_ids]
-    payload["content"] = _base64(content)
+    payload["external_ids"] = [_b64encode(_id) for _id in external_ids]
+    payload["content"] = _b64encode(content)
     return json.dumps(payload)
 
 # Create new blockchain and return the chain id
@@ -29,20 +36,37 @@ def create_new_chain(external_ids, temp):
                json=_json_block_payload(external_ids, content), headers=headers)
     return response.json()["chain_id"]
 
+# Unit tests 
 class FactomAPITest(unittest.TestCase):
 
-    def test_base64(self):
+    # Test base 64 encode
+    def test_b64encode(self):
+        data = "foobar"
         expected = "Zm9vYmFy"
-        output = _base64("foobar")
+        output = _b64encode(data)
+        print("\nInput = {} \nOutput = {}".format(data, output))
+        self.assertEqual(expected, output)
+
+    # Test base 64 decode
+    def test_b64decode(self):
+        data = "Zm9vYmFy"
+        expected = "foobar"
+        output = _b64decode(data)
+        print("\nInput = {} \nOutput = {}".format(data, output))
         self.assertEqual(expected, output)
     
+    # Test unix timestamp
     def test_unix_timestamp(self):
-        print("datetime = {}".format(unix_timestamp()))
+        print("\nDateTime = {}".format(_unix_timestamp()))
         self.assertTrue(True)
 
+    # Test json block payload
     def test_json_block_payload(self):
+        data = (["foo", "bar"], "content")
         expected = {"external_ids" : ["Zm9v", "YmFy"], "content" : "Y29udGVudA=="}
-        output = _json_block_payload(["foo", "bar"], "content")
+        output = _json_block_payload(*data)
+        print("\nInput = {} \nOutput = {}".format(data, json.loads(output)))
+
         self.assertEqual(expected, json.loads(output))
 
 if __name__ == "__main__":
