f2a3d7f9be4af72130c753434c5c4a624302c30f
Akash Kwatra <akashkw@gmail.com>
2018-06-09 23:50:21 -0500

    implement add_to_chain and get_entry

--- factom.py
+++ factom.py
@@ -23,18 +23,29 @@ def _b64decode(str):
 def _unix_timestamp():
     return '{:%Y-%m-%d-%H-%M-%S}'.format(datetime.datetime.now())
 
-# Create json payload for a block using external_ids and content
-def _json_block_payload(external_ids, content):
+# Create payload for a block using external_ids and content
+def _block_payload(external_ids, content):
     payload = dict()
     payload['external_ids'] = [_b64encode(_id) for _id in external_ids]
     payload['content'] = _b64encode(content)
     return payload
 
-# Create new blockchain and return the chain id
+# Create new blockchain and return the chain_id and entry_hash
 def create_new_chain(external_ids, content):
     response = requests.post("{}/chains".format(API),
-               json=_json_block_payload(external_ids, content), headers=HEADERS)
-    return response.json()["chain_id"]
+               json=_block_payload(external_ids, content), headers=HEADERS)
+    return response.json()
+
+# Add to block chain with chain_id and return entry_hash
+def add_to_chain(chain_id, external_ids, content):
+    response = requests.post("{}/chains/{}/entries".format(API, chain_id),
+               json=_block_payload(external_ids, content), headers=HEADERS)
+    return response.json()
+
+# Get entry specified entry_hash of block, can specify hash, first, or last by default
+def get_entry(chain_id, entry_hash="last"):
+    response = requests.get("{}/chains/{}/entries/first", headers=HEADERS)
+    return response.json() 
 
 # Unit tests 
 class FactomAPITest(unittest.TestCase):
@@ -60,11 +71,11 @@ class FactomAPITest(unittest.TestCase):
         print("\nDateTime = {}".format(_unix_timestamp()))
         self.assertTrue(True)
 
-    # Test json block payload
-    def test_json_block_payload(self):
+    # Test block payload
+    def test_block_payload(self):
         data = (["foo", "bar"], "content")
         expected = {"external_ids" : ["Zm9v", "YmFy"], "content" : "Y29udGVudA=="}
-        output = _json_block_payload(*data)
+        output = _block_payload(*data)
         print("\nInput = {} \nOutput = {}".format(data, output))
         self.assertEqual(expected, output)
 
