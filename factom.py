26a5ad8b385295b63091b96b84af8085b1e0eded
Akash Kwatra <akashkw@gmail.com>
2018-06-09 21:01:38 -0500

    implement create chain and unittests

--- factom.py
+++ factom.py
@@ -1,9 +1,47 @@
 import requests
 import base64 
+import datetime
 import json
+import unittest
+# import nosetest
 
 API = 'https://apiplus-api-sandbox-testnet.factom.com/v1/'
 
 KEY = 'GUWiB4x3JwrOiaWyFMYLlRmfJr7YfFFGDMzwYG80sdtL4BDT'
 
+HEADER  = {'Content-Type': 'application/json', 'factom-provider-token': KEY }
+
+def _base64(str):
+    return base64.b64encode(str.encode('utf-8')).decode('utf-8')
+
+def unix_timestamp():
+    return "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
+
+def _json_block_payload(external_ids, content):
+    payload = dict()
+    payload["external_ids"] = []
+
+# Create new blockchain and return the chain id
+def create_new_chain(external_ids, temp):
+    response = requests.post("{}/chains".format(API),
+               json=_json_block_payload(external_ids, content), headers=headers)
+    return response.json()["chain_id"]
+
+class FactomAPITest(unittest.TestCase):
+
+    def test_base64(self):
+        expected = "Zm9vYmFy"
+        output = _base64("foobar")
+        self.assertEqual(expected, output)
+    
+    def test_unix_timestamp(self):
+        print("datetime = {}".format(unix_timestamp()))
+        self.assertTrue(True)
+
+    def test_json_block_payload(self):
+        print("Test not implemented")
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
 
